8f87755866bcbca70c7d0438abbf204fc5822651
AJYURH <ayran_andrade@hotmail.com>
2019-07-01 14:59:45 -0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#import Tkinter as tkinter
 import tkinter
 import matplotlib.pyplot as plt
 import PIL.Image
@@ -7,26 +8,27 @@ import PIL.ImageTk
 import time
 import threading
 import serial
+from socket import *
 
 #GUI Root Window
 
 top = tkinter.Tk()
-top.title("Projetobô")
+top.title("Projetobo")
 top.resizable(width = 0, height = 0)
 
 #GUIvariables
 
 decoded_bytes = 'a'
 
-#LIMPAR VARIAVEIS GLOBAIS
+#Limpar Variaves Globais
 
 robotStatus = ''
-speedX = 0.0
-speedY = 0.0
-speedT = 0.0
-recievedSpeedX = 0.0
-recievedSpeedY = 0.0
-recievedSpeedΘ = 0.0
+speedV = 0.0
+speedO = 0.0
+#speedT = 0.0
+recievedSpeedV = 0.0
+#recievedSpeedY = 0.0
+recievedSpeedO = 0.0
 poseX = 0.0
 poseY = 0.0
 poseT = 0.0
@@ -41,6 +43,11 @@ tempS1 = [20,24,38,39,30,32,37,21,22,29]
 tempS2 = [0,2,4,9,0,0,0,0,0,0]
 tempS3 = [0,2,4,9,0,0,0,0,0,0]
 tempS4 = [0,2,4,9,0,0,0,0,0,0]
+speedSV = [0,0,0,0,0,0,0,0,0,0]
+speedSO = [0,0,0,0,0,0,0,0,0,0]
+poseSX = [0,0,0,0,0,0,0,0,0,0]
+poseSY = [0,0,0,0,0,0,0,0,0,0]
+poseST = [0,0,0,0,0,0,0,0,0,0]
 photo = PIL.ImageTk.PhotoImage(PIL.Image.open('rob.jpg'))
 manualOn = (False)
 
@@ -53,7 +60,7 @@ receivingPositionF = (False)
 
 #Begin GUI
 
-#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#
+###########################################################################################
 
 
 #updater
@@ -63,14 +70,15 @@ def updater():
 
     global decoded_bytes
 
-    #speedY = 0.0
     robotStatus = ''
-    speedX = 0.0
-    speedY = 0.0
-    speedT = 0.0
-    poseX = 0.0
-    poseY = 0.0
-    poseT = 0.0
+    #speedY = 0.0
+    #robotStatus = ''
+    speedV = 0.0
+    #speedY = 0.0
+    speedO = 0.0
+    #poseX = 0.0
+    #poseY = 0.0
+    #poseT = 0.0
     temp1 = 0.0
     temp2 = 0.0
     temp3 = 0.0
@@ -86,92 +94,95 @@ def updater():
         temp1 = timeB[9]+990
 
 
+        #Begin Temp
+
+        ############################################################
+        '''
+        serverSocket = socket(AF_INET, SOCK_DGRAM)
+        portAdress = ('192.168.043.033', 5555)
+        serverSocket.bind(portAdress)
+        print('waiting')
+        tempvalues = serverSocket.recvfrom(1024)
+        print(tempvalues)
+        '''
+        ############################################################
+
+        #End Temp
+
+
         #Beguin Serial COM
 
-        #☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#
+        ###########################################################################################
 
 
         #UNCOMENT
-        '''
-        ser = serial.Serial('/dev/ttyACM0') #set correct port
-        ser.flushInput()
-        '''
+        
+        #ser = serial.Serial('/dev/ttyACM0') #set correct port
+        ser = serial.Serial()
+        ser.port = 'COM12'
+        ser.boudrate = 9600
+        ser.open()
+        #ser.flushInput()
+        print("open?")
+             
         #UNCOMENT ABOVE
 
         
         try:
-            #ser_bytes = ser.readline() #UNCOMENT
-            #decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))  #UNCOMENT
-            #print(decoded_bytes)
+            ser_bytes = ser.readline() #UNCOMENT
+            print("open?")
+            decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))  #UNCOMENT
             print(decoded_bytes)
-            #decoded_bytes = str('1-684.125-688.111-879.548-215.358-511.010-152.548')
+            print(decoded_bytes)
+            #decoded_bytes = str('1-684.125-688.111-879.548-215.358-511.010')
             
             if decoded_bytes[0] == '0':
                 robotStatus = 'Aguardando'          
             elif decoded_bytes[0] == '1':
+                print(decoded_bytes)
                 robotStatus = 'Movimentando'   
                 poseX = float(decoded_bytes[2:9])
                 poseY = float(decoded_bytes[10:17])
                 poseT = float(decoded_bytes[18:25])
-                speedX = float(decoded_bytes[26:33])
-                speedY = float(decoded_bytes[34:41])     
+                speedV = float(decoded_bytes[26:33])
+                #speedO = float(decoded_bytes[34:41])     
             elif decoded_bytes[0] == '2':
                 robotStatus = 'Rotacionando'
                 poseX = float(decoded_bytes[2:9])
                 poseY = float(decoded_bytes[10:17])
                 poseT = float(decoded_bytes[18:25])
-                speedT = float(decoded_bytes[42:49])    
+                speedO = float(decoded_bytes[34:41])    
             else:
                 print('Invalid Command')
 
                 '''
                 #FOR FREEEEEEEEEEE CONTROOOOOOOOOL! =DDDD
 
-                poseX = float(decoded_bytes[2:8])
-                poseY = float(decoded_bytes[10:16])
-                poseT = float(decoded_bytes[18:24])
-                speedX = float(decoded_bytes[26:32])
-                speedY = float(decoded_bytes[34:40])
-                speedT = float(decoded_bytes[42:48])
+                poseX = float(decoded_bytes[2:9])
+                poseY = float(decoded_bytes[10:17])
+                poseT = float(decoded_bytes[18:25])
+                speedV = float(decoded_bytes[26:33])
+                speedO = float(decoded_bytes[34:41])
+                #speedT = float(decoded_bytes[42:48])
                 '''
 
-            '''
-            with open ("test_data.cvs", "a") as f:
-                writer = cvs.writer(f, delimiter = ',')
-                writer.writerow([time.time(), decoded_bytes])
-                if receivingStatusF == False and receivingPositionF == False and receivingSpeedF == False:
-
-                    if(decoded_bytes == 'status'):
-                        receivingStatusF = True
-                        while(receivingStatusF == True):                        
-                            try:
-                                ser_bytes = ser.readline()
-                    elif(decoded_bytes == 'position'):
-                        receivingPositionF = True
-                    elif(decoded_bytes == 'speed'):
-                        receivingSpeedF = True
-                    else:
-                        print('invalid value')
-                        break
-                else:
-                    print('invalid supervisor condition')
-            '''                
         except:
             print('No Serial')
 
-    #☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#
+    ###########################################################################################
 
     #End Serial COM
 
 
         #Update Robot Data
         
+        robotStatusL = tkinter.Label (box1, anchor = 'w', text = "Status de Rob:"+'%s' %(robotStatus), padx = 2)
         #decoded_bytes = ('1-325.548')
         #speedX = float(decoded_bytes[2]+decoded_bytes[3]+decoded_bytes[4]+decoded_bytes[5]+decoded_bytes[6]+decoded_bytes[7]+decoded_bytes[8])
         #print(speedX)
-        speedXValue = tkinter.Label(robotDataBox, text = '%s' % (speedX))
-        speedYValue = tkinter.Label(robotDataBox, text = '%s' % (speedY))
-        speedTValue = tkinter.Label(robotDataBox, text = '%s' % (speedT))  
+        speedVValue = tkinter.Label(robotDataBox, text = '%s' % (speedV))
+        speedOValue = tkinter.Label(robotDataBox, text = '%s' % (speedO))
+        #speedTValue = tkinter.Label(robotDataBox, text = '%s' % (speedT))  
 
         #Update Temp Data
                 
@@ -182,9 +193,10 @@ def updater():
 
         #Speed Updated Robot Data
 
-        speedXValue.grid(row = 1, column = 2, sticky ='E',padx=(0,0))
-        speedYValue.grid(row = 2, column = 2, sticky ='E')
-        speedTValue.grid(row = 3, column = 2, sticky ='E')
+        robotStatusL.grid(row = 1, column = 1, sticky = 'W', pady = (4,0))
+        speedVValue.grid(row = 1, column = 2, sticky ='E',padx=(0,0))
+        speedOValue.grid(row = 2, column = 2, sticky ='E')
+        #speedTValue.grid(row = 3, column = 2, sticky ='E')
 
         #Speed Updated Temp Data
 
@@ -203,35 +215,75 @@ def graph():
     fig = plt.gcf()
     #plt.interactive(True)
     #plt.clf()
-    ax1 = fig.add_subplot(221)
+    ax1 = fig.add_subplot(331)
     plt.title('Sensor de Temperatura 1')
     #ax1.plot(timeB,tempS1)
     plt.xlabel('tempo(s)')
-    plt.ylabel('Temperatura (C°)')
+    plt.ylabel('Temperatura (C)')
     #plt.grid(True)
     #ax1.axis([timeB[0], timeB[9], 20, 40])
     #plt.figure(2)
-    ax2 = fig.add_subplot(222)
+    ax2 = fig.add_subplot(332)
     plt.title('Sensor de Temperatura 2')
     ax2.plot(timeB,tempS2)
     plt.xlabel('tempo(s)')
-    plt.ylabel('Temperatura (C°)')
+    plt.ylabel('Temperatura (C)')
     #plt.grid(True)
     plt.axis([timeB[0], timeB[9], 20, 40])
     #plt.figure(3)
-    ax3 = fig.add_subplot(223)
+    ax3 = fig.add_subplot(333)
     plt.title('Sensor de Temperatura 3')
     ax3.plot(timeB,tempS3)
     plt.xlabel('tempo(s)')
-    plt.ylabel('Temperatura (C°)')
+    plt.ylabel('Temperatura (C)')
     #plt.grid(True)
     plt.axis([timeB[0], timeB[9], 20, 40])
     #plt.figure(4)
-    ax4 = fig.add_subplot(224)
+    ax4 = fig.add_subplot(334)
     plt.title('Sensor de Temperatura 4')
     ax4.plot(timeB,tempS4)
     plt.xlabel('tempo(s)')
-    plt.ylabel('Temperatura (C°)')
+    plt.ylabel('Temperatura (C)')
+    #plt.grid(True)
+    plt.axis([timeB[0], timeB[9], 20, 40])
+    #plt.figure(5)
+    ax5 = fig.add_subplot(335)
+    plt.title('Velocidade V/t')
+    ax5.plot(timeB,speedSV)
+    plt.xlabel('tempo(s)')
+    plt.ylabel('VelV')
+    #plt.grid(True)
+    plt.axis([timeB[0], timeB[9], 20, 40])
+    #plt.figure(6)
+    ax6 = fig.add_subplot(336)
+    plt.title('Velocidade T/t')
+    ax6.plot(timeB,speedSO)
+    plt.xlabel('tempo(s)')
+    plt.ylabel('VelT')
+    #plt.grid(True)
+    plt.axis([timeB[0], timeB[9], 20, 40])
+    #plt.figure(7)
+    ax7 = fig.add_subplot(337)
+    plt.title('PoseX/t')
+    ax7.plot(timeB,poseSX)
+    plt.xlabel('tempo(s)')
+    plt.ylabel('PoseX')
+    #plt.grid(True)
+    plt.axis([timeB[0], timeB[9], 20, 40])
+    #plt.figure(8)
+    ax8 = fig.add_subplot(338)
+    plt.title('PoseY/t')
+    ax8.plot(timeB,poseSY)
+    plt.xlabel('tempo(s)')
+    plt.ylabel('PoseY')
+    #plt.grid(True)
+    plt.axis([timeB[0], timeB[9], 20, 40])
+    #plt.figure(9)
+    ax9 = fig.add_subplot(339)
+    plt.title('PoseT/t')
+    ax9.plot(timeB,poseST)
+    plt.xlabel('tempo(s)')
+    plt.ylabel('PoseT')
     #plt.grid(True)
     plt.axis([timeB[0], timeB[9], 20, 40])
     plt.tight_layout()
@@ -279,26 +331,26 @@ def openManual():
 
     def input():
         global decoded_bytes
-        recievedSpeedX = float(e1.get())
-        #recievedSpeedY = float(e2.get())
-        recievedSpeedΘ = float(e3.get())
+        recievedPoseX = float(e1.get())
+        #recievedPoseY = float(e2.get())
+        recievedPoseT = float(e3.get())
         
-        speedX = float(e1.get()) #DEBUGGING
+        #speedV = float(e1.get()) #DEBUGGING
         decoded_bytes = str(e2.get())
         
         print('adsdasd %s' % decoded_bytes)
         #check if float, and size, if True and acceptable send
         
-        print('value a: %s \nvalue b: %s \nvalue c: %s' % (recievedSpeedX, recievedSpeedY, recievedSpeedΘ))
+        print('value a: %s \nvalue b: %s' % (recievedPoseX, recievedPoseT))
 
     #Manual Window
       
         #Labels
 
     iControlL = tkinter.Label (manualW, anchor = 'w', text = "Inserir TODOS os valores de controle:" , padx = 2)
-    iSpeedX = tkinter.Label (manualW, anchor = 'w', text = "Speed X", padx = 2)
-    iSpeedY = tkinter.Label (manualW, anchor = 'w', text = "Speed Y", padx = 2)
-    iSpeedΘ = tkinter.Label (manualW, anchor = 'w', text = "Speed Θ", padx = 2)
+    iPoseX = tkinter.Label (manualW, anchor = 'w', text = "Pose X", padx = 2)
+    iPoseY = tkinter.Label (manualW, anchor = 'w', text = "Pose Y", padx = 2)
+    iPoseT = tkinter.Label (manualW, anchor = 'w', text = "Pose T", padx = 2)
 
         #input boxes
 
@@ -314,9 +366,9 @@ def openManual():
         #Positioning Manual
 
     iControlL.grid(row = 1, column = 1, pady = 2, padx = 3, sticky = 'W')
-    iSpeedX.grid(row = 2, column = 1, pady = 2, padx = 3, sticky = 'W')
-    iSpeedY.grid(row = 3, column = 1, pady = 2, padx = 3, sticky = 'W')
-    iSpeedΘ.grid(row = 4, column = 1, pady = 2, padx = 3, sticky = 'W')
+    iPoseX.grid(row = 2, column = 1, pady = 2, padx = 3, sticky = 'W')
+    iPoseY.grid(row = 3, column = 1, pady = 2, padx = 3, sticky = 'W')
+    iPoseT.grid(row = 4, column = 1, pady = 2, padx = 3, sticky = 'W')
     e1.grid(row = 2, column = 2, pady = 2, padx = 3, sticky = 'E')
     e2.grid(row = 3, column = 2, pady = 2, padx = 3, sticky = 'E')
     e3.grid(row = 4, column = 2, pady = 2, padx = 3, sticky = 'E')
@@ -373,12 +425,12 @@ robotData = tkinter.Label (box1,  text = "Dados de Rob", anchor = 'w', padx = 2)
 tempData = tkinter.Label(box1, anchor = 'w', text = "Dados de Temperatura", padx = 2)
 
                 #RoboData
-speedXL = tkinter.Label(robotDataBox, text = "Robot speed x:")
-speedYL = tkinter.Label(robotDataBox, text = "Robot speed y:")
-speedTL = tkinter.Label(robotDataBox, text = "Robot speed Θ:" )
-speedXValue = tkinter.Label(robotDataBox, text = '%s' % (speedX))
-speedYValue = tkinter.Label(robotDataBox, text = '%s' % (speedY))
-speedTValue = tkinter.Label(robotDataBox, text = '%s' % (speedT))
+speedVL = tkinter.Label(robotDataBox, text = "Robot speed v:")
+speedOL = tkinter.Label(robotDataBox, text = "Robot speed o:")
+#speedTL = tkinter.Label(robotDataBox, text = "Robot speed Θ:" )
+speedVValue = tkinter.Label(robotDataBox, text = '%s' % (speedV))
+speedOValue = tkinter.Label(robotDataBox, text = '%s' % (speedO))
+#speedTValue = tkinter.Label(robotDataBox, text = '%s' % (speedT))
 poseXL = tkinter.Label(robotDataBox, text = "Robot position x:")
 poseYL = tkinter.Label(robotDataBox, text = "Robot position y:")
 poseTL = tkinter.Label(robotDataBox, text = "Robot position Θ:")
@@ -418,12 +470,12 @@ tempDataBox.grid(row = 5, column =1,sticky = 'W')
 
                 #RoboData
 
-speedXL.grid(row = 1, column = 1, sticky ='W')
-speedYL.grid(row = 2, column = 1, sticky ='W')
-speedTL.grid(row = 3, column = 1, sticky ='W')
-speedXValue.grid(row = 1, column = 2, sticky ='E',padx=(100,0))
-speedYValue.grid(row = 2, column = 2, sticky ='E')
-speedTValue.grid(row = 3, column = 2, sticky ='E')
+speedVL.grid(row = 1, column = 1, sticky ='W')
+speedOL.grid(row = 2, column = 1, sticky ='W')
+#speedTL.grid(row = 3, column = 1, sticky ='W')
+speedVValue.grid(row = 1, column = 2, sticky ='E',padx=(100,0))
+speedOValue.grid(row = 2, column = 2, sticky ='E')
+#speedTValue.grid(row = 3, column = 2, sticky ='E')
 poseXL.grid(row = 4, column = 1, sticky ='W')
 poseYL.grid(row = 5, column = 1, sticky ='W')
 poseTL.grid(row = 6, column = 1, sticky ='W')
@@ -457,7 +509,7 @@ photoF.grid(row = 1, column = 2, sticky = 'E')
 
 top.mainloop()
 
-#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯#
+###########################################################################################
 
 #End GUI
 
